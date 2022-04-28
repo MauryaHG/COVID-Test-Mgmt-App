57082dee6df6f2c0a2ec2d3c3b17b05939071873
mgam0010 <mgam0010@student.monash.edu>
2022-04-28 20:46:46 +1000

    added booking class to store user data from bookingview page

--- src/main/java/com/example/demo/BookingView.java
+++ src/main/java/com/example/demo/BookingView.java
@@ -1,19 +1,23 @@
 package com.example.demo;
 
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.UI;
+import com.example.demo.users.Booking;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.Route;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Route("Booking")
 
@@ -27,6 +31,9 @@ public class BookingView extends VerticalLayout {
     EmailField email = new EmailField("Email");
     DatePicker bookingDate = new DatePicker("Booking Date");
     TextField siteId = new TextField("Site ID");
+
+    private Button bookButton = new Button("Book");
+    private ArrayNode errorsLayout;
 
     public BookingView(){
 
@@ -46,13 +53,61 @@ public class BookingView extends VerticalLayout {
         formLayout.add(bookingDate, siteId);
 
         TextField taskField = new TextField();
-        Button bookButton = new Button("Book");
+
         add(bookButton);
         bookButton.addClickListener(click -> {
         });
-
         layout.setSizeFull();
         layout.setAlignItems(Alignment.CENTER);
 
+        buildForm();
     }
+    private Component buildForm(){
+
+        Binder<Booking> binder = new Binder<>(Booking.class);
+        binder.forField(firstName)
+                .asRequired("Name is required")
+                .bind(Booking::getAdditionalInfo, Booking::setAdditionalInfo);
+
+        binder.forField(lastName)
+                .asRequired("Name is required")
+                .bind(Booking::getAdditionalInfo, Booking::setAdditionalInfo);
+
+        binder.forField(bookingDate)
+                .asRequired("date is required")
+                .bind(Booking::getStartTime, Booking::setStartTime);
+
+        binder.forField(siteId)
+                .asRequired("site ID is required")
+                .bind(Booking::getTestingSiteId, Booking::setTestingSiteId);
+
+        binder.readBean(new Booking());
+
+        binder.addStatusChangeListener(status -> {
+                    boolean emptyFields = Stream.of("name", "quantity", "snack")
+                            .flatMap(prop -> binder.getBinding(prop).stream())
+                            .anyMatch(binding -> binding.getField().isEmpty());
+                    bookButton.setEnabled(!emptyFields);
+                }
+
+        );
+
+        bookButton.addClickListener(click -> {
+            try {
+
+                Booking newBooking = new Booking();
+                binder.writeBean(newBooking);
+                //addOrder(newBooking); (3)
+                binder.readBean(new Booking());
+                System.out.println(newBooking);
+            } catch (ValidationException e) {
+                errorsLayout.add(String.valueOf(new Html(e.getValidationErrors().stream()
+                        .map(res -> "<p>" + res.getErrorMessage() + "</p>")
+                        .collect(Collectors.joining("\n")))));
+            }
+        });
+        return null;
+    }
+
+
 }
---
+++ src/main/java/com/example/demo/users/Booking.java
@@ -0,0 +1,58 @@
+package com.example.demo.users;
+
+import java.time.LocalDate;
+
+
+public class Booking {
+
+
+    private String customerId;
+    private String testingSiteId;
+    private LocalDate startTime;
+    private String notes;
+    private String additionalInfo;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getTestingSiteId() {
+        return testingSiteId;
+    }
+
+    public void setTestingSiteId(String testingSiteId) {
+        this.testingSiteId = testingSiteId;
+    }
+
+    public LocalDate getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDate startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+
+    public Booking() {
+
+    }
+}
